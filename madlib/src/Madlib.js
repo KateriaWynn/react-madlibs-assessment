aab4da90a545178bd5daaaac340ec123b351ec77
Kateria Wynn <kwynn1211@gmail.com>
2021-06-23 23:31:40 -0400

    created Madlib component

---
+++ madlib/src/Madlib.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import MadLibForm from './MadlibForm';
+import './styles.css';
+import './styles-custom.css';
+
+function Madlib() {
+  return (
+    <div>
+      <MadLibForm />
+    </div>
+  );
+}
+export default Madlib;
