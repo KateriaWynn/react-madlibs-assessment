40cb1354bbe16cadbbc08419eb66b8bfbdf680f1
Kateria Wynn <kwynn1211@gmail.com>
2021-06-25 01:11:35 -0400

    updated file

--- madlib/src/Madlib.js
+++ madlib/src/Madlib.js
@@ -1,45 +1,179 @@
 import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import TextInput from './TextInput';
+import Select from './Select';
 import './styles.css';
 import './styles-custom.css';
+import Story from './Story';
 
-/* Renders a Mad lib */
+/* Renders a Mad lib form using Formik */
 
-function Madlib({ formValues }) {
+const Madlib = () => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
-    <div className="madlib">
-      <p>Sit or lay down in a comfortable way.</p>
-
-      <p>
-        Close your eyes or gently look down. Take {formValues.breathsOne} deep
-        belly breathes in and out. Imagine you are traveling to{' '}
-        {formValues.nounOne}. You take a look around and see colorful{' '}
-        {formValues.nounTwo} and sparkling {formValues.nounThree}.
-      </p>
-      <p>
-        You notice how {formValues.moodOne} you are feeling. You take a deep
-        breath in through your nose and smell {formValues.nounFour}.
-      </p>
-
-      <p>
-        Suddenly you look to your left and see {formValues.nounFive}. Mmmm. You
-        take a bite and the food tastes delicious.
-      </p>
-      <p>
-        Then you look to your right and see {formValues.nounSix}. It's very
-        friendly and comes right up to you. You pick it up and give it a gentle
-        snuggle.
-      </p>
-      <p>
-        You notice how {formValues.moodTwo} you feel. Your body feels calm from
-        your head to your toes.
-      </p>
-      <p>
-        You take one more look around this amazing place and take{' '}
-        {formValues.breathsTwo} deep, belly breaths in and out.
-      </p>
-
-      <p>Now slowly open your eyes and notice how your body feels.</p>
-    </div>
+    <>
+      <Formik
+        initialValues={{
+          breathsOne: '',
+          nounOne: '',
+          nounTwo: '',
+          nounThree: '',
+          moodOne: '',
+          nounFour: '',
+          nounFive: '',
+          nounSix: '',
+          moodTwo: '',
+          breathsTwo: '',
+        }}
+        validationSchema={Yup.object({
+          breathsOne: Yup.string()
+            .oneOf(['1', '2', '3', '4'], 'Invalid choice')
+            .required('Required'),
+          nounOne: Yup.string().required('Required'),
+          nounTwo: Yup.string().required('Required'),
+          nounThree: Yup.string().required('Required'),
+          moodOne: Yup.string()
+            .oneOf(['happy', 'calm', 'peaceful'], 'Invalid choice')
+            .required('Required'),
+          nounFour: Yup.string().required('Required'),
+          nounFive: Yup.string().required('Required'),
+          nounSix: Yup.string().required('Required'),
+          moodTwo: Yup.string()
+            .oneOf(['content', 'relaxed', 'calm'], 'Invalid choice')
+            .required('Required'),
+          breathsTwo: Yup.string()
+            .oneOf(['1', '2', '3', '4'], 'Invalid choice')
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setTimeout(() => {
+            setSubmitting(false);
+            resetForm();
+          }, 60000);
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+          <form onSubmit={handleSubmit}>
+            <div className={isSubmitting ? 'hidden' : 'madlib-form'}>
+              <h1>Mad Libs Guided Imagery</h1>
+              <p>A mindfulness exercise by Chicago Mind</p>
+              <Select
+                name="breathsOne"
+                value={values.breathsOne}
+                onChange={handleChange}
+                onBlur={handleBlur('nounOne')}
+              >
+                <option value="">Choose a number</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </Select>
+              <TextInput
+                name="nounOne"
+                type="text"
+                placeholder="Name a relaxing place"
+                onChange={handleChange}
+                value={values.nounOne}
+                onBlur={handleBlur('nounOne')}
+              />
+              <TextInput
+                name="nounTwo"
+                type="text"
+                placeholder="Name one thing in your relaxing place"
+                onChange={handleChange}
+                value={values.nounTwo}
+                onBlur={handleBlur('nounTwo')}
+              />
+              <TextInput
+                name="nounThree"
+                type="text"
+                placeholder="Name another thing in your relaxing place"
+                onChange={handleChange}
+                value={values.nounThree}
+                onBlur={handleBlur('nounThree')}
+              />
+              <Select
+                name="moodOne"
+                value={values.moodOne}
+                onChange={handleChange}
+                onBlur={handleBlur('moodOne')}
+              >
+                <option value="">Choose a word</option>
+                <option value="happy">happy</option>
+                <option value="calm">calm</option>
+                <option value="peaceful">peaceful</option>
+              </Select>
+              <TextInput
+                name="nounFour"
+                type="text"
+                placeholder="Name a soothing or favorite smell"
+                onChange={handleChange}
+                value={values.nounFour}
+                onBlur={handleBlur('nounFour')}
+              />
+              <TextInput
+                name="nounFive"
+                type="text"
+                placeholder="Name a favorite food"
+                onChange={handleChange}
+                value={values.nounFive}
+                onBlur={handleBlur('nounFive')}
+              />
+              <TextInput
+                name="nounSix"
+                type="text"
+                placeholder="Name your favorite animal with fur"
+                onChange={handleChange}
+                value={values.nounSix}
+                onBlur={handleBlur('nounSix')}
+              />
+              <Select
+                name="moodTwo"
+                value={values.moodTwo}
+                onChange={handleChange}
+                onBlur={handleBlur('moodTwo')}
+              >
+                <option value="">Choose a word</option>
+                <option value="content">content</option>
+                <option value="relaxed">relaxed</option>
+                <option value="calm">calm</option>
+              </Select>
+              <Select
+                name="breathsTwo"
+                value={values.breathsTwo}
+                onChange={handleChange}
+                onBlur={handleBlur('breathsTwo')}
+              >
+                <option value="">Choose a number</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </Select>
+              <div className="submit">
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </div>
+            </div>
+            <div className={!isSubmitting ? 'hidden' : 'madlib'}>
+              <Story formValues={values} />
+              <div className="refresh-button">
+                <button type="button" onClick={refreshPage}>
+                  Restart
+                </button>
+              </div>
+            </div>
+          </form>
+        )}
+      </Formik>
+    </>
   );
-}
+};
+
 export default Madlib;
